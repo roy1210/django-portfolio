62fdc6f979680578eedd904c882b8a0ca4f7d4e7
roy1210 <rswkure@gmail.com>
2019-05-23 06:43:09 +0800

    set static floder

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,13 @@ class Blog(models.Model):
     pub_data = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        # returns only slice of 100words
+        return self.body[:100]
+
+    def pub_data_pretty(self):
+        return self.pub_data.strftime('%b %e %Y')
--- blog/urls.py
+++ blog/urls.py
@@ -17,5 +17,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # path '': `blog/`
     path('', views.allblogs, name='allblogs'),
+    path('<int:blog_id>', views.detail, name='detail'),
 ]
