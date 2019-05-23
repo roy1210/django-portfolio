270eb3f0a41d804b76ff86af91bb96576904d9bb
roy1210 <rswkure@gmail.com>
2019-05-24 04:32:40 +0800

    local_settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import portfolio.local_settings
+try:
+    import portfolio.local_settings
+except ImportError:
+    pass
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
