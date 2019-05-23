faf00ee01b2d02479f6718f88307f4efddd23fbc
roy1210 <rswkure@gmail.com>
2019-05-24 04:39:03 +0800

    settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,10 +11,17 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+
+# Hosting Server setting
 try:
-    import portfolio.local_settings
+    from server_settings.py import *
 except ImportError:
-    pass
+    import portfolio.local_settings
+
+# try:
+#     import portfolio.local_settings
+# except ImportError:
+#     pass
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
